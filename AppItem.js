42e65f38ffca87c4c633d71b4e16009a687ec82f
Notorious-James <142017086+Notorious-James@users.noreply.github.com>
2024-06-17 20:11:06 -0300

    Create AppItem.js

---
+++ AppItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Eu estou arrasando na programação com Expo Ass: Thiago Elias</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
